399860883fe12ebf7924b4d9b1d7c227838324fb
f-marinelli <fabri.marinelli@gmail.com>
2022-05-21 16:28:32 +0200

    refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,6 @@
-import { useEffect, useState } from 'react';
-// import styles from '../styles/Home.module.css';
 import Articolo from '../components/Articolo';
 
 export default function Home({ articoli }) {
-  const [articoliLista, setArticoliLista] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await fetch(
-  //       `https://notizen-e76c1-default-rtdb.firebaseio.com/articoli.json`
-  //     );
-  //     const body = await result.json();
-  //     setArticoliLista(Object.values(body));
-  //     console.log('//');
-  //   };
-  //   fetchData();
-  // }, []);
-
-  // const articoli =
-  //   articoliLista.length > 1
-  //     ? articoliLista.map((art, i) => (
-  //         <Articolo key={i} testo={art.testo} titolo={art.titolo} />
-  //       ))
-  //     : [];
-
   const listaArticoli = articoli.map((art, i) => (
     <Articolo key={i} testo={art.testo} titolo={art.titolo} />
   ));
