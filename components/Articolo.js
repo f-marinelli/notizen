6d17254a1d62351c5d898f1602d76518540053d0
f-marinelli <fabri.marinelli@gmail.com>
2022-05-21 15:53:55 +0200

    test getStaticProp

--- components/Articolo.js
+++ components/Articolo.js
@@ -1,8 +1,8 @@
 const Articolo = (prop) => {
   return (
-    <div className="card" style="width: 18rem;">
+    <div className="card" style={{ width: '13rem' }}>
       <div className="card-body">
-        <h5 className="card-title">{props.titolo}</h5>
+        <h5 className="card-title">{prop.titolo}</h5>
         <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
         <p className="card-text">{prop.testo}</p>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -2,28 +2,50 @@ import { useEffect, useState } from 'react';
 // import styles from '../styles/Home.module.css';
 import Articolo from '../components/Articolo';
 
-export default function Home() {
-  const [articoliLista, setArticoliLista] = useState({});
+export default function Home({ articoli }) {
+  const [articoliLista, setArticoliLista] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch(
-        `https://notizen-e76c1-default-rtdb.firebaseio.com/articoli.json`
-      );
-      const body = await result.json();
-      setArticoliLista(body);
-      console.log('//');
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await fetch(
+  //       `https://notizen-e76c1-default-rtdb.firebaseio.com/articoli.json`
+  //     );
+  //     const body = await result.json();
+  //     setArticoliLista(Object.values(body));
+  //     console.log('//');
+  //   };
+  //   fetchData();
+  // }, []);
 
-  useEffect(() => {
-    console.log(...Object.entries(articoliLista));
-  }, [articoliLista]);
+  // const articoli =
+  //   articoliLista.length > 1
+  //     ? articoliLista.map((art, i) => (
+  //         <Articolo key={i} testo={art.testo} titolo={art.titolo} />
+  //       ))
+  //     : [];
+
+  const listaArticoli = articoli.map((art, i) => (
+    <Articolo key={i} testo={art.testo} titolo={art.titolo} />
+  ));
 
   return (
     <>
       <h1>Homepage</h1>
+      {listaArticoli}
     </>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    'https://notizen-e76c1-default-rtdb.firebaseio.com/articoli.json'
+  );
+  const body = await res.json();
+  const articoli = Object.values(body);
+
+  return {
+    props: {
+      articoli,
+    },
+  };
 }
