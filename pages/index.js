e5d6c600467901198c78bb94168fa2090739e95b
f-marinelli <fabri.marinelli@gmail.com>
2022-05-21 16:43:46 +0200

    test cache-control

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,14 @@ export default function Home({ articoli }) {
   );
 }
 
-export async function getServerSideProps() {
-  const res = await fetch(
+export async function getServerSideProps({ req, res }) {
+  const response = await fetch(
     'https://notizen-e76c1-default-rtdb.firebaseio.com/articoli.json'
   );
-  const body = await res.json();
+  const body = await response.json();
   const articoli = Object.values(body);
+
+  res.setHeader('Cache-Control', 'public, s-maxage=120');
 
   return {
     props: {
