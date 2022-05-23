ff977a58f0819962ffce1f9d310ba35ad55a5bbd
f-marinelli <fabri.marinelli@gmail.com>
2022-05-23 16:47:04 +0200

    nested routing

--- components/Articolo.js
+++ components/Articolo.js
@@ -1,12 +1,21 @@
-const Articolo = (prop) => {
+import Link from 'next/link';
+
+const Articolo = (props) => {
   return (
-    <div className="card" style={{ width: '13rem' }}>
-      <div className="card-body">
-        <h5 className="card-title">{prop.titolo}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-        <p className="card-text">{prop.testo}</p>
+    <Link
+      href={{
+        pathname: `/articolo${props.id}`,
+        query: { id: props.id, titolo: props.titolo, testo: props.testo },
+      }}
+    >
+      <div className="card" style={{ width: '13rem' }}>
+        <div className="card-body">
+          <h5 className="card-title">{props.titolo}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p className="card-text">{props.testo}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
